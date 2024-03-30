41c12a98ef1a1631e485eb44adff5891b12b2df1
li-0406 <cr870406@gmail.com>
2024-03-30 17:07:29 +0800

    Updates

--- static/recommend.CYw4Gpsz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./entry.BAMT0CE_.js";import{k as o,o as t,F as s}from"./swiper-vue.INM7Fvbn.js";const c={},n={class:"bg-gradient-to-br from-white to-secondary-100"},r=s("h1",{class:"text-3xl font-en underline"},"recommend",-1),a=[r];function _(d,i){return t(),o("div",n,a)}const l=e(c,[["render",_]]);export{l as default};
---
+++ static/recommend.Cqv08BXv.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.Bu-dXOEI.js";import{k as o,o as t,F as s}from"./swiper-vue.INM7Fvbn.js";const c={},n={class:"bg-gradient-to-br from-white to-secondary-100"},r=s("h1",{class:"text-3xl font-en underline"},"recommend",-1),a=[r];function _(d,i){return t(),o("div",n,a)}const l=e(c,[["render",_]]);export{l as default};
