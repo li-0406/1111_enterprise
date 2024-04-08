8f2b5d81f208752fb12f62c39a797cef8ddb0873
li-0406 <cr870406@gmail.com>
2024-04-08 20:52:10 +0800

    Updates

---
+++ static/registration.Beso5yKn.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.Cytib3zb.js";import{k as t,o,F as s}from"./swiper-vue.INM7Fvbn.js";const r={},n={class:"bg-gradient-to-br from-white to-secondary-100"},c=s("h1",{class:"text-3xl font-en underline"},"registration",-1),a=[c];function i(_,d){return o(),t("div",n,a)}const m=e(r,[["render",i]]);export{m as default};
